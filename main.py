bab2bd1e6160b01af484ef1f36291f720271b1fe
Gokalp Erol <gokalperol022@gmail.com>
2022-10-24 21:03:34 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    stuff = "K1 <b>sex</b>"
+    computer_mark = ["asus", "lenovo", "casper", "monster"]
+    return render_template('index.html',
+                           stuff=stuff,
+                           computer_mark=computer_mark, )
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+
+@app.errorhandler(404)
+def page_404_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_500_internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=1337)
