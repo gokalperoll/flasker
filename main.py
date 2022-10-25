993ae7b299e867247610894d03cd6ca4a85967f0
Gokalp Erol <gokalperol022@gmail.com>
2022-10-25 23:26:10 +0300

    New html is name.html

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "mysupersecretkey"
+
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route('/')
 def index():
-    title="Index"
+    title = "Index"
     stuff = "K1 <b>sex</b>"
     computer_mark = ["asus", "lenovo", "casper", "monster"]
     return render_template('index.html',
                            stuff=stuff,
                            computer_mark=computer_mark,
-                           title=title,)
+                           title=title, )
 
 
 @app.route('/user/<name>')
@@ -27,6 +37,20 @@ def page_404_not_found(e):
 @app.errorhandler(500)
 def page_500_internal_server_error(e):
     return render_template('500.html'), 500
+
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('name.html',
+                           name=name,
+                           form=form,)
 
 
 if __name__ == '__main__':
